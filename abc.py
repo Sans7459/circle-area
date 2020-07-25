1cb5c14bdc32a01afdcf0d4f292bea03e7208d65
Sans7459 <68772352+Sans7459@users.noreply.github.com>
2020-07-25 14:42:48 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,7 @@
+
+>>> from math import pi
+>>> r= float(input("Input the radius of the circle :"))
+Input the radius of the circle :1.1
+>>> print("The area of the circle with radius " + str(r) + "is: " + str(pi*r**2))
+The area of the circle with radius 1.1is: 3.8013271108436504
+>>> 
